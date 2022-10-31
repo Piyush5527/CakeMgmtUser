5b89ca753921eb2e5ce0074eb603026bcb8407e1
Piyush5527 <piyushjainkgp36@gmail.com>
2022-10-31 18:06:57 +0530

    initial commit

---
+++ app/src/main/java/com/example/cakeshopper/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.cakeshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // as soon as the application opens the first
+        // fragment should be shown to the user
+        // in this case it is algorithm fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
+    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.Home) {
+            selectedFragment = new Home();
+        } else if (itemId == R.id.Cakes) {
+            selectedFragment = new Cakes();
+        } else if (itemId == R.id.Cart) {
+            selectedFragment = new Cart();
+        }
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
+}
